12c461f9618779e7e1a5204282306a4ba5f71252
Ajitesh13 <aji.yash13@gmail.com>
2020-05-19 13:47:10 +0530

    server.js configures for deployment

--- server/server.js
+++ server/server.js
@@ -24,15 +24,13 @@ app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
-app.get("/api/config/paypal", (req, res) => {
-    res.send(config.PAYPAL_CLIENT_ID);
-})
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static("/../client/build"));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    });
-}
+// app.get("/api/config/paypal", (req, res) => {
+//     res.send(config.PAYPAL_CLIENT_ID);
+// })
+app.use(express.static(path.join(__dirname, '/../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+});
 
 
 
