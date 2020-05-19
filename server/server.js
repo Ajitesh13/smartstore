f5a3a1f0a44c597ba10d80defe3d9739a3149ee6
Ajitesh13 <aji.yash13@gmail.com>
2020-05-19 13:00:32 +0530

    configured for deployment

--- server/server.js
+++ server/server.js
@@ -27,10 +27,13 @@ app.use("/api/orders", orderRoute);
 app.get("/api/config/paypal", (req, res) => {
     res.send(config.PAYPAL_CLIENT_ID);
 })
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-});
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static("/../client/build"));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
+}
+
 
 
 const port = 5000;
